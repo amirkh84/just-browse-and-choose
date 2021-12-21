c1b3c2184275157927038f622da9780047ad2579
Amirkhodadadi <amirkhodadadi0084@gmail.com>
2021-12-21 15:07:48 +0330

    added 1 column to colors and improve hex code display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ function add() {
     let bb = document.body;
     let dd = document.createElement("div");
     dd.classList.add("box");
-    bb.innerHTML += ("<div class = 'box' ><div class = 'color1'><p class = 'hex'></p></div><div class = 'color2'><p class='hex2'></div></div>");
+    bb.innerHTML += ("<div class = 'box' ><div class = 'color1'><p class = 'hex'></p></div><div class = 'color1'><p class='hex'></div><div class = 'color1'><p class='hex'></div></div>");
 };
 //this will add color plattes till the "ii" equals to the "load number"
 let load = 100;
@@ -12,21 +12,20 @@ for (let ii = 0; ii < load; ii++) {
 };
 //variables
 let color1 = document.getElementsByClassName("color1");
-let color2 = document.getElementsByClassName("color2");
 let hex = document.getElementsByClassName("hex");
-let hex2 = document.getElementsByClassName("hex2");
 //the hex color generator
 function gen() {
-    const vv = Math.floor(Math.random()*16777215).toString(16);
-    const vvv = "#" + vv;
-    return vvv;
+    let symbols , color;
+    symbols = "0123456789ABCDEF";
+    color = "#";
+    for (let ic = 0; ic<6; ic++) {
+        color = color + symbols[Math.floor(Math.random() * 16 )];
+    };
+    return color;
 };
-//adding hex codes to color boxes and show the hex code
+//adding colors and hex codes
 for (let i = 0; i < color1.length; i++) {
     let getcolor = gen();
     color1[i].setAttribute("style" , "background-color:" + getcolor);
     hex[i].innerText = getcolor;
-    let getcolor2 = gen();
-    color2[i].setAttribute("style" , "background-color:" + getcolor2);
-    hex2[i].innerText = getcolor2;
 };
